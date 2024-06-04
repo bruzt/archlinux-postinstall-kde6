790fed224b55f66a2e563b89f454f39218179077
Bruno Zutim <bzutim@gmail.com>
2024-06-04 19:07:37 -0300

    Update archlinux-postinstall-kde6.sh

--- archlinux-postinstall-kde6.sh
+++ archlinux-postinstall-kde6.sh
@@ -58,7 +58,7 @@ function archKde6 {
   bash -c 'echo "vm.max_map_count=16777216" >> /etc/sysctl.d/99-sysctl.conf'
 
   flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  flatpak install -y com.github.tchx84.Flatseal org.onlyoffice.desktopeditors com.github.wwmm.easyeffects org.kde.kdenlive com.heroicgameslauncher.hgl net.davidotek.pupgui2 com.obsproject.Studio #org.videolan.VLC
+  flatpak install -y com.github.tchx84.Flatseal org.onlyoffice.desktopeditors com.github.wwmm.easyeffects org.kde.kdenlive com.heroicgameslauncher.hgl net.davidotek.pupgui2 com.obsproject.Studio it.mijorus.gearlever #org.videolan.VLC
   flatpak install -y com.leinardi.gst io.github.thetumultuousunicornofdarkness.cpu-x
 
   ### DEV
