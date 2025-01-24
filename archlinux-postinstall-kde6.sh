5b582ae3c06b47eb720dc55d1d0293dbdbef381a
Bruno Zutim <bzutim@gmail.com>
2025-01-23 21:09:00 -0300

    Update archlinux-postinstall-kde6.sh

--- archlinux-postinstall-kde6.sh
+++ archlinux-postinstall-kde6.sh
@@ -27,7 +27,7 @@ function archKde6 {
 
   pacman -S --noconfirm --needed discover packagekit-qt6 fwupd colord-kde kimageformats kdeplasma-addons
   pacman -S --noconfirm --needed flatpak xdg-desktop-portal-kde xdg-desktop-portal-gtk
-  pacman -S --noconfirm --needed partitionmanager filelight kolourpaint haruna kamoso ttf-droid noto-fonts-emoji net-tools
+  pacman -S --noconfirm --needed partitionmanager filelight kolourpaint haruna kamoso qalculate-qt ttf-droid noto-fonts-emoji net-tools
   pacman -S --noconfirm --needed plasma-firewall ufw
   #pacman -S --noconfirm --needed timeshift
 
@@ -61,7 +61,7 @@ function archKde6 {
   #bash -c 'echo "vm.max_map_count=16777216" >> /etc/sysctl.d/99-sysctl.conf'
 
   flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  flatpak install -y com.github.tchx84.Flatseal org.kde.kalk org.onlyoffice.desktopeditors com.github.wwmm.easyeffects com.dropbox.Client #org.videolan.VLC
+  flatpak install -y com.github.tchx84.Flatseal org.onlyoffice.desktopeditors com.github.wwmm.easyeffects com.dropbox.Client #org.videolan.VLC
   flatpak install -y com.heroicgameslauncher.hgl net.davidotek.pupgui2 org.kde.kdenlive com.obsproject.Studio
   flatpak install -y com.leinardi.gst io.github.thetumultuousunicornofdarkness.cpu-x
 
